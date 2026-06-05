4116fc9f25ec62efe636ec042830316c071821ea
lvargascur-stack <lvargascur@gmail.com>
2026-06-04 19:58:48 -0500

    Create MesaDAO.java
    Agrega MesaDAO con Javadoc

---
+++ src/dao/leo/MesaDAO.java
@@ -0,0 +1,59 @@
+package dao.leo;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Clase para manejar el acceso a datos de Mesa.
+ * @author Vargas Curisinche, Leonel Wilverth
+ * @version 1.0
+ */
+public class MesaDAO {
+
+    /**
+     * Lista todas las mesas registradas en el sistema.
+     * @return Lista de objetos Mesa
+     */
+    public static List<Mesa> listarMesas() {
+        List<Mesa> mesas = new ArrayList<>();
+        String sql = "SELECT * FROM Mesa";
+        try (Connection conn = ConexionBD.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery()) {
+            while (rs.next()) {
+                Mesa mesa = new Mesa();
+                mesa.setIdMesa(rs.getInt("idMesa"));
+                mesa.setNumeroMesa(rs.getInt("numeroMesa"));
+                mesa.setCapacidadMesa(rs.getInt("capacidadMesa"));
+                mesa.setEstadoMesa(rs.getString("estadoMesa"));
+                mesas.add(mesa);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return mesas;
+    }
+
+    /**
+     * Cambia el estado de una mesa (disponible/ocupada/reservada).
+     * @param idMesa Identificador de la mesa
+     * @param nuevoEstado Nuevo estado de la mesa
+     * @return true si se actualizó correctamente, false en caso contrario
+     */
+    public static boolean cambiarEstadoMesa(int idMesa, String nuevoEstado) {
+        String sql = "UPDATE Mesa SET estadoMesa = ? WHERE idMesa = ?";
+        try (Connection conn = ConexionBD.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, nuevoEstado);
+            pstmt.setInt(2, idMesa);
+            return pstmt.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
