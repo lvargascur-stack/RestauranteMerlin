0554cdd12acc55857dc7c47b6436646a043f0100
Eli-vr <U21318256@utp.edu.pe>
2026-06-06 11:38:05 -0500

    Update Usuario.java
    arreglo

--- src/model/elizabeth/Usuario.java
+++ src/model/elizabeth/Usuario.java
@@ -10,22 +10,37 @@ package model.elizabeth;
  */
 public class Usuario {
     private int idUsuario;
-    private String nombreUsuario;
+    private String usuario;
     private String password;
     private String rol;
+    private String estado;
     
     public Usuario() { }
+    
+    public Usuario(int idUsuario, String usuario, String password, String rol, String estado) {
+    this.idUsuario = idUsuario;
+    this.usuario = usuario;
+    this.password = password;
+    this.rol = rol;
+    this.estado = estado;
+}
     
     
     
     public int getIdUsuario() { return idUsuario; }
     public void setIdUsuario(int idUsuario) { this.idUsuario = idUsuario; }
-    public String getNombreUsuario() { return nombreUsuario; }
-    public void setNombreUsuario(String nombreUsuario) { this.nombreUsuario = nombreUsuario; }
+    
+    public String getNombreUsuario() { return usuario; }
+    public void setNombreUsuario(String nombreUsuario) { this.usuario = nombreUsuario; }
+    
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
+    
     public String getRol() { return rol; }
     public void setRol(String rol) { this.rol = rol; }
+    
+    public String getEstado() { return estado; }
+    public void setEstado(String estadoUsuario) { this.estado = estadoUsuario; }
     
     
 }
