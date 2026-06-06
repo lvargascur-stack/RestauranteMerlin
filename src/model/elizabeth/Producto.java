9d239854163807997785b388723fa599035fce86
Eli-vr <U21318256@utp.edu.pe>
2026-06-06 12:15:32 -0500

    Update Producto.java
    Producto

--- src/model/elizabeth/Producto.java
+++ src/model/elizabeth/Producto.java
@@ -11,19 +11,39 @@ package model.elizabeth;
 public class Producto {
     
     private int idProducto;
-    private String nombre;
+    private String nombreProducto;
+    private String descripcion;
     private double precio;
     private int stock;
+    private int idCategoria;
     
     public Producto() {}
     
+    public Producto(int idProducto, String nombreProducto, String descripcion, double precio, int stock, int idCategoria){
+        this.idProducto= idProducto;
+        this.nombreProducto= nombreProducto;
+        this.descripcion= descripcion;
+        this.precio= precio;
+        this.stock= stock;
+        this.idCategoria= idCategoria;
+    }
+    
     public int getIdProducto() { return idProducto; }
     public void setIdProducto(int idProducto) { this.idProducto = idProducto; }
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
+    
+    public String getNombreProducto() { return nombreProducto; }
+    public void setNombreProducto(String nombreProducto) { this.nombreProducto = nombreProducto; }
+    
+    public String getDescripcion() {return descripcion;}
+    public void setDescripcion(String descripcion) {this.descripcion= descripcion;}
+    
     public double getPrecio() { return precio; }
     public void setPrecio(double precio) { this.precio = precio; }
+    
     public int getStock() { return stock; }
     public void setStock(int stock) { this.stock = stock; }
+    
+    public int gestIdCategoria() {return idCategoria;}
+    public void setIdCategoria(int idCategoria) {this. idCategoria = idCategoria;}
     
 }
