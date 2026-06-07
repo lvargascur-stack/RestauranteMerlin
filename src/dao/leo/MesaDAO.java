6e0a47a4c813d71b327aba500eb187414fdaef80
Eli-vr <U21318256@utp.edu.pe>
2026-06-07 13:40:07 -0500

    Update MesaDAO.java
    agregue import.model.elizabeth.Mesa

--- src/dao/leo/MesaDAO.java
+++ src/dao/leo/MesaDAO.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import model.elizabeth.Mesa;
 
 /**
  * Clase para manejar el acceso a datos de Mesa.
